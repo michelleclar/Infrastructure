48e19c5ef40edbc1a22137063f4a3650ab9183ca
carl <carlmichelle493@gmail.com>
2025-01-05 18:56:27 +0800

    add comment

---
+++ src/main/java/org/carl/infrastructure/comment/Utils.java
@@ -0,0 +1,21 @@
+package org.carl.infrastructure.comment;
+
+import jakarta.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import org.jboss.logging.Logger;
+
+public class Utils {
+    private static final Logger log = Logger.getLogger(Utils.class);
+
+    public static String hashMD5(String source) {
+        String res = null;
+        try {
+            var messageDigest = MessageDigest.getInstance("MD5");
+            var mdBytes = messageDigest.digest(source.getBytes());
+            res = DatatypeConverter.printHexBinary(mdBytes);
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        return res;
+    }
+}
