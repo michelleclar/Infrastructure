fcafdfbc8a792e139b3ac7342d56c12fa030046f
carl <carlmichelle493@gmail.com>
2025-02-07 13:19:37 +0800

    change name send message to broadcast

--- infrastructure/src/main/java/org/carl/infrastructure/ability/ISentMessageAbility.java
+++
@@ -1,8 +0,0 @@
-package org.carl.infrastructure.ability;
-
-import org.carl.infrastructure.message.ISendMessageOperations;
-
-// TODO: complete
-public interface ISentMessageAbility {
-    ISendMessageOperations getSearchOperations();
-}
