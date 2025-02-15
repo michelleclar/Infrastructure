7f82c67d05f008a9125a9461f79ce554cd130b9e
carl <carlmichelle493@gmail.com>
2025-02-15 19:01:35 +0800

    scaffold

--- infrastructure/src/main/java/org/carl/infrastructure/PersistenceScaffold.java
+++ infrastructure/src/main/java/org/carl/infrastructure/PersistenceScaffold.java
@@ -2,31 +2,27 @@ package org.carl.infrastructure;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
-import org.carl.infrastructure.core.ability.IPersistenceAbility;
+
 import org.carl.infrastructure.core.ability.ITableCreateAbility;
 import org.carl.infrastructure.persistence.IPersistenceOperations;
+import org.carl.infrastructure.persistence.PersistenceStd;
 import org.jboss.logging.Logger;
 
 /** why use set inject bean because I think better copy by this write */
-public abstract class PersistenceScaffold implements IPersistenceAbility {
+public abstract class PersistenceScaffold extends PersistenceStd {
 
     static final Logger log = Logger.getLogger(PersistenceScaffold.class);
-    private IPersistenceOperations persistenceOperations;
+    @Inject IPersistenceOperations operations;
 
     @Inject
-    public void setPersistenceOperations(IPersistenceOperations databaseOperations) {
-        this.persistenceOperations = databaseOperations;
-    }
-
-    @Override
-    public IPersistenceOperations getPersistenceOperations() {
-        return persistenceOperations;
+    public void setPersistenceOperations(IPersistenceOperations operations) {
+        this.operations = operations;
     }
 
     @PostConstruct
     protected void init() {
         if (this instanceof ITableCreateAbility ability) {
-            ability.create(getPersistenceOperations());
+            ability.create(operations);
         }
         afterInit();
         log.infof("{} initialized", this.getClass().getSimpleName());
