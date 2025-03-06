1fa565f3b5aaf1c4d5a37342fc17e99044753869
carl <carlmichelle493@gmail.com>
2025-03-06 15:40:29 +0800

    db ctx

--- oidc/db/ctx_test.go
+++ oidc/db/ctx_test.go
@@ -3,13 +3,11 @@ package db
 import (
 	"context"
 	"log"
+	"oidc/ent/migrate"
+	"oidc/ent/user"
 	"testing"
 )
 
-func TestProcessString(t *testing.T) {
-	//str := ProcessString("hello %v", "workd")
-	//println(str)
-}
 func TestCtx(t *testing.T) {
 	ctx := context.Background()
 	users, err := Ctx().User.Query().All(ctx)
@@ -25,8 +23,43 @@ func TestCreateTable(t *testing.T) {
 		log.Fatal("failed creating schema resources", err)
 	}
 }
+
+func TestAutomatic(t *testing.T) {
+	if err := Ctx().Schema.Create(context.Background(), migrate.WithDropColumn(true), migrate.WithDropIndex(true), migrate.WithForeignKeys(false)); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	ctx := context.Background()
+	u, err := Ctx().User.Create().SetAge(30).SetName("a8m").Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating user: %s", err)
+	}
+	log.Println(u)
+}
+
 func TestSearch(t *testing.T) {
 	ctx := context.Background()
-	Ctx().User.Query().Select()
-	Ctx().User.Query().Where(U).First(ctx)
+	items, err := Ctx().User.Query().Select(user.FieldUserID).Where(user.AgeEQ(30)).All(ctx)
+	if err != nil {
+		log.Fatalf("failed searching users: %s", err)
+	}
+	log.Println(items)
+}
+
+func TestUpdate(t *testing.T) {
+	ctx := context.Background()
+
+	items := Ctx().User.Query().Select(user.FieldUserID).Where(user.AgeEQ(30)).AllX(ctx)
+	for _, i := range items {
+		u := Ctx().User.Update().Where(user.ID(i.ID)).SetAge(18).SaveX(ctx)
+		log.Println(u)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	ctx := context.Background()
+	affected := Ctx().User.Delete().Where(user.ID(123)).ExecX(ctx)
+	log.Println(affected)
 }
