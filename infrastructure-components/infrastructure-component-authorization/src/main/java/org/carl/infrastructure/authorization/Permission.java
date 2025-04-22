45598c642203c00352b117585415e41abed7f332
carl <carlmichelle493@gmail.com>
2025-04-22 12:46:14 +0800

    feat: add auth

---
+++ infrastructure-components/infrastructure-component-authorization/src/main/java/org/carl/infrastructure/authorization/Permission.java
@@ -0,0 +1,6 @@
+package org.carl.infrastructure.authorization;
+
+/** 权限基础类，用于表示系统中的权限 */
+public interface Permission {
+    String getName();
+}
---
+++ infrastructure-components/infrastructure-component-authorization/src/main/java/org/carl/infrastructure/authorization/Role.java
@@ -0,0 +1,6 @@
+package org.carl.infrastructure.authorization;
+
+/** 角色基础类，用于表示系统中的角色 */
+public interface Role {
+    String getName();
+}
