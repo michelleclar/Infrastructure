c50ef5aa4988a2d57623a29c232002dd614b24ff
carl <carlmichelle493@gmail.com>
2025-05-11 11:42:15 +0800

    delete : module vector

--- infrastructure-components/infrastructure-component-vector/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-//    implementation(project(":infrastructure-components:infrastructure-component-tool"))
-    implementation(libs.bundles.web)
-    implementation(project(":infrastructure-component-qdrant-grpc"))
-}
