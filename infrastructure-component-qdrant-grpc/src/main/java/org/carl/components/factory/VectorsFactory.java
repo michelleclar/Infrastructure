df814496149204a1c287335bc7b65ddc272bef29
carl <carlmichelle493@gmail.com>
2025-05-22 18:02:26 +0800

    feat: add qdrant factor

---
+++ infrastructure-component-qdrant-grpc/src/main/java/org/carl/components/factory/VectorsFactory.java
@@ -0,0 +1,56 @@
+package org.carl.components.factory;
+
+import static org.carl.components.factory.VectorFactory.vector;
+
+import io.qdrant.client.grpc.Points.NamedVectors;
+import io.qdrant.client.grpc.Points.Vector;
+import io.qdrant.client.grpc.Points.Vectors;
+import java.util.List;
+import java.util.Map;
+
+/** Convenience methods for constructing {@link Vectors} */
+public final class VectorsFactory {
+    private VectorsFactory() {}
+
+    /**
+     * Creates named vectors
+     *
+     * @param values A map of vector names to {@link Vector}
+     * @return a new instance of {@link Vectors}
+     */
+    public static Vectors namedVectors(Map<String, Vector> values) {
+        return Vectors.newBuilder()
+                .setVectors(NamedVectors.newBuilder().putAllVectors(values))
+                .build();
+    }
+
+    /**
+     * Creates a vector
+     *
+     * @param values A list of values
+     * @return a new instance of {@link Vectors}
+     */
+    public static Vectors vectors(List<Float> values) {
+        return Vectors.newBuilder().setVector(vector(values)).build();
+    }
+
+    /**
+     * Creates a vector
+     *
+     * @param values A list of values
+     * @return a new instance of {@link Vectors}
+     */
+    public static Vectors vectors(float... values) {
+        return Vectors.newBuilder().setVector(vector(values)).build();
+    }
+
+    /**
+     * Creates a vector
+     *
+     * @param vector An instance of {@link Vector}
+     * @return a new instance of {@link Vectors}
+     */
+    public static Vectors vectors(Vector vector) {
+        return Vectors.newBuilder().setVector(vector).build();
+    }
+}
